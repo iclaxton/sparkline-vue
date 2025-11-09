1ba927da8182c0806495b23966e974d4c40f2b68
Ian Claxton <ian_claxton@hotmail.com>
2025-11-09 13:22:54 +0000

    feat: add jsconfig.json for TypeScript configuration and include paths

--- index.js
+++ index.js
@@ -21,5 +21,5 @@ if (typeof window !== 'undefined' && window.Vue) {
   window.Vue.use(SparklinePlugin);
 }
 
-export default Sparkline;
+// Use ONLY named exports (no default)
 export { Sparkline, SparklinePlugin };
